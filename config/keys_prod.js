8013efae36d310233fc63c8f48429d991d3ab43a
kgchicobava <kgchicobava@gmail.com>
2019-01-17 14:30:19 +0200

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
 	mongoURI: "mongodb://admin:12131013007a@ds145083.mlab.com:45083/mern",
-	secretOrKey: process.env.SECRET_OR_KEY
+	secretOrKey: "SOMETIMES DIFFERENT IS THE SAME"
 };
