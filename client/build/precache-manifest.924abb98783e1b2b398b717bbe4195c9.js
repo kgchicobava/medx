437c04b42013697a04350d68bf39768290ba3074
kgchicobava <kgchicobava@gmail.com>
2019-01-17 14:27:58 +0200

    build

---
+++ client/build/precache-manifest.924abb98783e1b2b398b717bbe4195c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8158db486955afa7da80",
+    "url": "/static/js/main.8158db48.chunk.js"
+  },
+  {
+    "revision": "f41b87dde5edaade34ed",
+    "url": "/static/js/1.f41b87dd.chunk.js"
+  },
+  {
+    "revision": "8158db486955afa7da80",
+    "url": "/static/css/main.be8c4667.chunk.css"
+  },
+  {
+    "revision": "d0939b73c29c8d9fbdd838562c39f40d",
+    "url": "/index.html"
+  }
+];
