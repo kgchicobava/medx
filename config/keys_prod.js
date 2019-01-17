5d22efb741d0f7d45402e88417a1bc40b5302e57
Roman Demyan <kgchicobava@gmail.com>
2019-01-17 13:57:44 +0200

    dpl3

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: "mongodb://admin:12131013007a@ds145083.mlab.com:45083/mern",
-	secretOrKey: "SOMETIMES DIFFERENT IS THE SAME"
+	mongoURI: process.env.MONGO_URI,
+	secretOrKey: process.env.SECRET_OR_KEY
 };
